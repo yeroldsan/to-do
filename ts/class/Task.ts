98482eabe0225c65cd34005d7adc87c5b227a8d8
doteins <112283864+doteins@users.noreply.github.com>
2023-03-27 16:43:12 +0300

    Add 'Task' class

---
+++ ts/class/Task.ts
@@ -0,0 +1,13 @@
+class Task {
+    id: number
+    description: string
+    completed: boolean
+
+    constructor(id:number, description:string, completed: boolean) {
+        this.id = id
+        this.description= description
+        this.completed = completed
+    }
+}
+
+export { Task }
