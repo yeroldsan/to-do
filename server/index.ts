5304c5ea084ee632d84627abf6d66d036ce3ea12
doteins <112283864+doteins@users.noreply.github.com>
2023-03-29 14:15:32 +0300

    Add db connection

--- server/index.ts
+++ server/index.ts
@@ -16,17 +16,17 @@ const port: number = 3001;
 // Create connection with the database
 const openDB = (): Pool => {
     const pool: Pool = new Pool ({
-        user: "postgres",
-        host: "localhost",
-        database: "todo",
-        password: "1234",
-        port: 5432
-        // user: "root",
-        // host: "dpg-cggp5tu4daddcg550ivg-a.frankfurt-postgres.render.com",
-        // database: "todo_aiux",
-        // password: "Dp7lxWkQlr9Z1PXdojZIyuf8Q1Br3d57",
-        // port: 5432,
-        // ssl: true
+        // user: "postgres",
+        // host: "localhost",
+        // database: "todo",
+        // password: "1234",
+        // port: 5432
+        user: "root",
+        host: "dpg-cggp5tu4daddcg550ivg-a.frankfurt-postgres.render.com",
+        database: "todo_aiux",
+        password: "Dp7lxWkQlr9Z1PXdojZIyuf8Q1Br3d57",
+        port: 5432,
+        ssl: true
     })
     return pool
 }
