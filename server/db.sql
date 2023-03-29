cad22a98a44fcc160aa4b16b2f3260452bb4e72a
doteins <112283864+doteins@users.noreply.github.com>
2023-03-29 13:53:19 +0300

    Add db scheme

---
+++ server/db.sql
@@ -0,0 +1,14 @@
+drop database if exists todo;
+
+create database todo;
+
+-- \c todo;
+use todo
+
+create table task (
+    id serial primary key,
+    description varchar(255) not null
+);
+
+insert into task (description) values ("My test task");
+insert into task (description) values ("My another task");
