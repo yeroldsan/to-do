f65f6b77826390d2cee39adc5185d4776d671663
doteins <112283864+doteins@users.noreply.github.com>
2023-04-07 10:41:20 +0300

    Divide routes from index

---
+++ server/routes.ts
@@ -0,0 +1,13 @@
+import express, { Router } from "express";
+import controllers from "./controllers";
+
+const router: Router = express.Router()
+
+router
+  .get("/", controllers.getAllTasks)
+  .post("/new", controllers.createTask)
+  .delete("/delete/:id", controllers.deleteTask)
+  .put("/update/:id", controllers.updateTask)
+  .patch("/update-status/:id", controllers.statusTask)
+
+  export default router
