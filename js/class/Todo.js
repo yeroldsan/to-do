d1cb07ae58137eb6f4f30aaf42efbdbfd3eb8452
doteins <112283864+doteins@users.noreply.github.com>
2023-05-11 12:34:00 +0300

    Compiled code from TS folder

--- js/class/Todo.js
+++ js/class/Todo.js
@@ -18,134 +18,132 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _Todos_instances, _Todos_backend_url, _Todos_readJson, _Todos_addToArray, _Todos_updateTaskDescription, _Todos_removeTask, _Todos_markTaskAsDone;
+var _Todos_instances, _Todos_backend_url, _Todos_readJson, _Todos_addToArray, _Todos_removeTask, _Todos_updateTaskDescription, _Todos_markTaskAsDone;
 import { Task } from "./Task.js";
 class Todos {
     constructor(url) {
         _Todos_instances.add(this);
         this.tasks = [];
-        _Todos_backend_url.set(this, void 0);
-        // Fetching data from the backend making HTTP call
-        // JSON (array) received as response
+        _Todos_backend_url.set(this, "");
+        // Fetching data from the backend making HTTP call JSON (array) received as response
+        // getTask = async () => {
+        //   return new Promise(async (resolve, reject) => {
+        //     fetch(this.#backend_url)
+        //       .then((response) => response.json())
+        //       .then((data) => {
+        //         this.#readJson(data);
+        //         console.log(data);    
+        //         resolve(this.tasks);
+        //         console.log(this.tasks);
+        //       })
+        //       .catch((error) => {
+        //         reject(error);
+        //       });
+        //   });
+        // };
         this.getTask = () => __awaiter(this, void 0, void 0, function* () {
-            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-                fetch(__classPrivateFieldGet(this, _Todos_backend_url, "f"))
-                    .then(response => response.json())
-                    .then((data) => {
-                    __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_readJson).call(this, data);
-                    resolve(this.tasks);
-                })
-                    .catch((error) => {
-                    reject(error);
-                });
-            }));
+            try {
+                const response = yield fetch(__classPrivateFieldGet(this, _Todos_backend_url, "f"));
+                const data = yield response.json();
+                __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_readJson).call(this, data);
+                return this.tasks;
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error('Error getting tasks from server');
+            }
         });
         this.addTask = (description) => __awaiter(this, void 0, void 0, function* () {
-            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+            try {
                 const json = JSON.stringify({ description: description });
-                fetch(__classPrivateFieldGet(this, _Todos_backend_url, "f") + "/new", {
-                    method: "post",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: json
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                    resolve(__classPrivateFieldGet(this, _Todos_instances, "m", _Todos_addToArray).call(this, data.id, description, data.comleted));
-                })
-                    .catch((error) => {
-                    reject(error);
-                    alert(error);
-                });
-            }));
-        });
-        this.removeTask = (id) => {
-            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-                fetch(__classPrivateFieldGet(this, _Todos_backend_url, "f") + "/delete/" + id, {
-                    method: "delete",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                })
-                    .then(response => response.json())
-                    .then((data) => {
-                    __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_removeTask).call(this, id);
-                    resolve(data.id);
-                })
-                    .catch((error) => {
-                    reject(error);
-                    alert(error);
-                });
-            }));
-        };
-        this.updateTask = (id, description) => {
-            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-                let json = JSON.stringify({ description: description });
-                fetch(__classPrivateFieldGet(this, _Todos_backend_url, "f") + "/update/" + id, {
-                    method: "put",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: json
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                    __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_updateTaskDescription).call(this, id, description, data.completed);
-                    resolve(data.description);
-                })
-                    .catch((error) => {
-                    reject(error);
-                });
-            }));
-        };
-        this.markTaskAsDone = (id, completed) => __awaiter(this, void 0, void 0, function* () {
-            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-                let json = JSON.stringify({ completed: completed });
-                fetch(__classPrivateFieldGet(this, _Todos_backend_url, "f") + "/update-status/" + id, {
-                    method: "put",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
+                const response = yield fetch(`${__classPrivateFieldGet(this, _Todos_backend_url, "f")}/new`, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
                     body: json,
-                })
-                    .then((response) => response.json())
-                    .then((data) => {
-                    __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_markTaskAsDone).call(this, id, completed);
-                    resolve(data.completed);
-                })
-                    .catch((error) => {
-                    reject(error);
                 });
-            }));
+                const data = yield response.json();
+                return __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_addToArray).call(this, data.id, description);
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error("Failed to get tasks from backend");
+            }
+        });
+        this.removeTask = (id) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                const response = yield fetch(`${__classPrivateFieldGet(this, _Todos_backend_url, "f")}/delete/${id}`, {
+                    method: "DELETE",
+                    headers: { "Content-Type": "application/json" }
+                });
+                __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_removeTask).call(this, id);
+                return id;
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error("Failed to delete task");
+            }
+        });
+        this.updateTaskDescription = (id, description, completed) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                const jsonData = JSON.stringify({ description: description, completed: completed });
+                const response = yield fetch(`${__classPrivateFieldGet(this, _Todos_backend_url, "f")}/update/${id}`, {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
+                    body: jsonData
+                });
+                const data = yield response.json();
+                __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_updateTaskDescription).call(this, data.id, data.description, data.completed);
+                return data.desccription;
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error("Failed to update task");
+            }
+        });
+        this.markTaskAsDone = (id, completed) => __awaiter(this, void 0, void 0, function* () {
+            try {
+                const jsonData = JSON.stringify({ completed: completed });
+                const response = yield fetch(`${__classPrivateFieldGet(this, _Todos_backend_url, "f")}/update-status/${id}`, {
+                    method: "PATCH",
+                    headers: { "Content-Type": "application/json" },
+                    body: jsonData
+                });
+                const data = yield response.json();
+                __classPrivateFieldGet(this, _Todos_instances, "m", _Todos_markTaskAsDone).call(this, data.id, data.completed);
+                return data.completed;
+            }
+            catch (error) {
+                console.error(error);
+                throw new Error("Failed to retrive task completed data");
+            }
         });
         __classPrivateFieldSet(this, _Todos_backend_url, url, "f");
     }
 }
 _Todos_backend_url = new WeakMap(), _Todos_instances = new WeakSet(), _Todos_readJson = function _Todos_readJson(tasksAsJson) {
-    tasksAsJson.forEach(element => {
+    tasksAsJson.forEach((element) => {
         const task = new Task(element.id, element.description, element.completed);
         this.tasks.push(task);
     });
-}, _Todos_addToArray = function _Todos_addToArray(id, description, completed) {
-    let task = new Task(id, description, completed);
+}, _Todos_addToArray = function _Todos_addToArray(id, description) {
+    const task = new Task(id, description, false);
     this.tasks.push(task);
     return task;
+}, _Todos_removeTask = function _Todos_removeTask(id) {
+    let updatedTasks = this.tasks.filter((task) => task.id !== id);
+    return updatedTasks;
 }, _Todos_updateTaskDescription = function _Todos_updateTaskDescription(id, description, completed) {
     let task = new Task(id, description, completed);
-    let index = this.tasks.findIndex(task => task.id === id);
+    let index = this.tasks.findIndex((task) => task.id === id);
     if (index >= 0) {
         this.tasks[index] = task;
     }
     return task;
-}, _Todos_removeTask = function _Todos_removeTask(id) {
-    let arrayWithoutRemovedTask = this.tasks.filter(task => task.id !== id);
-    this.tasks = arrayWithoutRemovedTask;
 }, _Todos_markTaskAsDone = function _Todos_markTaskAsDone(id, completed) {
-    let task = this.tasks.find((task) => task.id === id);
-    if (task) {
-        task.completed = true;
+    const taskToMark = this.tasks.find((task) => task.id === id);
+    if (taskToMark) {
+        taskToMark.completed = completed;
     }
-    return task;
+    return taskToMark;
 };
 export { Todos };
