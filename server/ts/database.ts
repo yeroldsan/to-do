5d1e3a742a59851924e4fd996ae5a072cb2223c1
doteins <112283864+doteins@users.noreply.github.com>
2023-05-13 22:49:31 +0300

    Create queryWithClient function

--- server/ts/database.ts
+++ server/ts/database.ts
@@ -1,53 +1,39 @@
 // Import required package
-import { Pool } from "pg";
+import { Pool, PoolClient, QueryResult } from "pg";
 import dotenv from "dotenv";
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Set up connection to the database
-const openDB = (): Pool => {
-    const pool: Pool = new Pool ({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        password: process.env.DB_PASSWORD,
-        port: Number(process.env.DB_PORT),
-        ssl: process.env.DB_SSL === 'true',
-    })
-    return pool
-}
-
-/*
-// Listen to the SIGINT and SIGTERM signals and close the database connection
-process.on('SIGINT', () => {
-    pool.end(() => {
-      console.log('Database connection closed due to application termination');
-      process.exit(0);
-    });
-  });
-  
-  process.on('SIGTERM', () => {
-    pool.end(() => {
-      console.log('Database connection closed due to application termination');
-      process.exit(0);
-    });
-  });
-*/
-
-// Connect to the database
-export const pool = openDB();
-
-
-// Class example!!!
-// export const query = async (sql:string, values:Array<any> = []): Promise<any> => {
-//  return new Promise(async (resolve, reject) => {
-//     try {
-//         const pool = openDB()
-//         const result = await pool.query(sql,values)
-//     }
-//     catch (error: any) {
-//         reject(error.message)
-//     }
-//  })   
+// const openDB = (): Pool => {
+  const pool: Pool = new Pool ({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    ssl: process.env.DB_SSL === 'true',
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  })
+//   return pool
 // }
+
+// Class example, adapted!
+export const queryWithClient = async (sql:string, values?: any[]): Promise<any> => {
+  // const pool = openDB()
+  // check out a single client
+  const client: PoolClient = await pool.connect()
+  try {
+    const result: QueryResult = await pool.query(sql,values)
+    return result
+}
+  catch (error: any) {
+    console.error(error.message);
+  } finally {
+    // release the client
+    await client.release()
+  }
+}
