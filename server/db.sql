de6f71620d207d08af524e64358d53bb0730f82d
doteins <112283864+doteins@users.noreply.github.com>
2023-06-06 16:22:37 +0300

    Update SQL syntax to follow SQL standard

--- server/db.sql
+++ server/db.sql
@@ -2,14 +2,14 @@ drop database if exists todo;
 
 create database todo;
 
--- \c todo;
-use todo
+\c todo; -- for PostgeSQL / use todo (MySQL)
 
 create table task (
-    id serial primary key,
-    description varchar(255) not null,
+    id int primary key generated always as identity, -- For PostgreSQL versions below 10, use SERIAL
+    description varchar(100) not null,
     completed boolean not null
 );
 
+-- Testing
 insert into task (description) values ("My test task");
 insert into task (description) values ("My another task");
